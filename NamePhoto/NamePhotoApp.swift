a278d2285ff2b5446e0e00108bf4a0a12907840e
mlwelker <nutria.watts.0g@icloud.com>
2023-09-19 15:39:06 -0500

    Initial Commit

---
+++ NamePhoto/NamePhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamePhotoApp.swift
+//  NamePhoto
+//
+//  Created by Michael Welker on 2023-09-19.
+//
+
+import SwiftUI
+
+@main
+struct NamePhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
