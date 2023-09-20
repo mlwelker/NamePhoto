2bd39e33a82f0118e8c9e61b84e823cc19d0c40c
mlwelker <nutria.watts.0g@icloud.com>
2023-09-19 23:20:29 -0500

    imageSaver

--- NamePhoto/ContentView.swift
+++ NamePhoto/ContentView.swift
@@ -15,6 +15,13 @@ struct ContentView: View {
             Button("Select Image") {
                 showingImagePicker = true
             }
+            
+            Button("Save Image") {
+                guard let inputImage else { return }
+                
+                let imageSaver = ImageSaver()
+                imageSaver.writeToPhotoAlbum(image: inputImage)
+            }
         }
         .padding()
         .sheet(isPresented: $showingImagePicker) {
