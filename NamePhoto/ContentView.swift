04d50a125f12fe8a9d7f2881103f7a6170bbcf16
mlwelker <nutria.watts.0g@icloud.com>
2023-09-19 22:58:11 -0500

    imagepicker coordinator

--- NamePhoto/ContentView.swift
+++ NamePhoto/ContentView.swift
@@ -3,18 +3,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
+    @State private var inputImage: UIImage?
     @State private var showingImagePicker = false
     
     var body: some View {
         VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
             Button("Select Image") {
                 showingImagePicker = true
             }
         }
         .padding()
         .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+            ImagePicker(image: $inputImage)
         }
+        .onChange(of: inputImage, loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage else { return }
+        image = Image(uiImage: inputImage)
     }
 }
 
